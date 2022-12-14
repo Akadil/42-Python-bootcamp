bf23589df68dd30dbc978e222b67f15b83b106f0
Your Name <akalimol@e2r8p16.clusters.42paris.fr>
2022-12-14 17:43:53 +0100

    Ex01 Module03 full exercise

--- module03/ex00/NumpyCreator.py
+++ module03/ex00/NumpyCreator.py
@@ -3,7 +3,54 @@ import numpy as np
 class NumpyCreator:
 
     def from_list(self, lst):
+
+        # Check if lst is list
+        if type(lst) is not list:
+            return None
+
+        # The basic case
+        if len(lst) == 1:
+            return np.array(lst)
+        
+        # Check for inconvenient shapes
+        same_length = len(lst[0])
+        for i in range(1, len(lst)):
+            if len(lst[i]) != same_length:
+                return None
+        
+        # Check for the types of the matrix inputs
+        type_of_input = type(lst[0][0])
+        for i in range(0, len(lst)):
+            for inp in lst[0]:
+                if type(inp) != type_of_input:
+                    return np.array(lst, dtype='s')
+
         return np.array(lst)
 
+
     def from_tuple(self, tpl):
+
+        # Check for the type
+        if type(tpl) is not tuple:
+            return None
+
         return np.array(tpl)
+
+
+    def from_iterable(self, itr):
+        return np.array(itr)
+
+    def from_shape(self, shape, value):
+        return np.full(shape, value)
+
+    def random(self, shape):
+        print("Hello world")
+
+    def identity(self, n):
+        return np.identity(n)
+
+"""
+https://www.w3schools.com/python/numpy/numpy_data_types.asp 
+The source for exploring the types of inputs
+
+"""
--- module06/ex01/tools.py
+++ module06/ex01/tools.py
@@ -6,11 +6,12 @@
 #    By: akalimol <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/12/13 18:43:06 by akalimol          #+#    #+#              #
-#    Updated: 2022/12/13 18:44:16 by akalimol         ###   ########.fr        #
+#    Updated: 2022/12/14 17:02:01 by akalimol         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 def add_intercept(x):
+    
     """Adds a column of 1’s to the non-empty numpy.array x.
     Args:
         x: has to be a numpy.array of dimension m * n.
@@ -22,3 +23,8 @@ def add_intercept(x):
         This function should not raise any Exception.
     """
     
+    len_x = x.shape[0]
+    list1 = [1 for i in range(len(x))]
+    np.concatenate([list1, x], axis=1)
+    x_1 = np.array([1 for i in range(x.shape[0])])
+    np.concantenate
