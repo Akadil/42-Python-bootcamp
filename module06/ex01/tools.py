e1e5adb2d58d75a0b2ec7231135ebd3e9e5793ce
Your Name <akalimol@e2r8p16.clusters.42paris.fr>
2022-12-14 16:08:44 +0100

    Ex01 modul06 modified

---
+++ module06/ex01/tools.py
@@ -0,0 +1,24 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tools.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: akalimol <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/12/13 18:43:06 by akalimol          #+#    #+#              #
+#    Updated: 2022/12/13 18:44:16 by akalimol         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def add_intercept(x):
+    """Adds a column of 1’s to the non-empty numpy.array x.
+    Args:
+        x: has to be a numpy.array of dimension m * n.
+    Returns:
+        X, a numpy.array of dimension m * (n + 1).
+        None if x is not a numpy.array.
+        None if x is an empty numpy.array.
+    Raises:
+        This function should not raise any Exception.
+    """
+    
